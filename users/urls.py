28aa200da78adf26a185a640d26b0683cc86f771
nochvev1@mail.ru <nochev1@mail.ru>
2022-09-12 22:05:15 +0300

    profile

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.register, name='reg'),
     path('user/', authViews.LoginView.as_view(template_name='users/user.html'), name='user'),
     path('exit/', authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
+    path('profile/', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 
 def register(request):
@@ -16,3 +17,7 @@ def register(request):
         form = UserRegisterForm
     return render(request, 'users/registration.html', {'form': form})
 
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
